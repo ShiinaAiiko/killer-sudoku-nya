7a1ae9fc2f0aceb610e8b6d59097acf369c22e32
ShiinaAiiko <aiiko_lan@qq.com>
2024-08-16 10:12:39 +0800

    backup_24-8-16

--- next.config.js
+++ next.config.js
@@ -5,15 +5,23 @@ const withPWA = require('next-pwa')({
 	dest: 'public',
 	register: true,
 	skipWaiting: true,
+	runtimeCaching: [],
+	publicExcludes: [], // like this
+	// publicExcludes: ['!**/*'], // like this
+	buildExcludes: [() => true],
+	fallbacks: false,
+	cacheStartUrl: true,
+	dynamicStartUrl: true,
+	disable: process.env.NODE_ENV === 'development',
 })
 // console.log(i18n)
 // console.log(i18n.i18n.locales)
 // console.log(i18n.i18n.locales.join(','))
 
-var enUS = require('./plugins/i18n/en-us.json')
-var jaJP = require('./plugins/i18n/ja-jp.json')
-var zhCN = require('./plugins/i18n/zh-cn.json')
-var zhTW = require('./plugins/i18n/zh-tw.json')
+// var enUS = require('./plugins/i18n/en-us.json')
+// var jaJP = require('./plugins/i18n/ja-jp.json')
+// var zhCN = require('./plugins/i18n/zh-cn.json')
+// var zhTW = require('./plugins/i18n/zh-tw.json')
 
 const nextConfig = withPWA({
 	// ...i18n,
